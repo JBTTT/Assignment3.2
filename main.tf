793f8dcbfa584c1bbb58b814499589aa79f3fa49
jibin <perseverancejbt@hotmail.com>
2025-10-29 01:52:52 +0800

    Assignment3.2

--- main.tf
+++ main.tf
@@ -1,23 +1,22 @@
-
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"
 }
 
 terraform {
-  backend "s3" {
-    bucket = "jibin-s3-tfstate-bucket989"
-    key    = "jibin-s3-tfstate-bucket989.tfstate"
-    region = "us-east-1"
-  }
+  backend "s3" {
+    bucket = "jibin-s3-tfstate-bucket989"
+    key    = "jibin-s3-tfstate-bucket989.tfstate"
+    region = "us-east-1"
+  }
 }
 
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+  bucket = "jibin-s3-tf-bkt-${local.account_id}"
 }
