47d177d6f48234bb1aa8bd4678915b975c8d27d7
jibin <perseverancejbt@hotmail.com>
2025-10-29 15:16:20 +0800

    Assignment3.2

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ terraform {
 
 }
 
-data "aws_caller_identity" "current" {}
+#data "aws_caller_identity" "current" {}
 
 #locals {
 #  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
